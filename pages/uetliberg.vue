4f63217af29d6016608ea6a00cca5d16c519a59c
stijnvermeeren <21952925+stijnvermeeren@users.noreply.github.com>
2025-04-28 21:19:31 +0200

    update Uetliberg legend (in progress)

--- pages/uetliberg.vue
+++ pages/uetliberg.vue
@@ -1,9 +1,31 @@
 <template>
   <h2>Trampelpfade am Uetliberg</h2>
 
-  <p>The Uetliberg (the 870m tall hill rising above Zürich) is full of fascinating and sometimes surprisingly challenging unofficial hiking trails ("Trampelpfade"). Documenting these has been the masterwork of <nuxt-link to="http://www.hikr.org/user/Uto869/">Martin "Uto869"</nuxt-link> on Hikr. Inspired by his work, I have made an interactive map of all of his routes and even some additional trails. (<nuxt-link to="http://www.hikr.org/post115832.html">Announcement on Hikr</nuxt-link>)</p>
+  <v-card>
+    <v-tabs v-model="language">
+      <v-tab value="en">English</v-tab>
+      <v-tab value="de">Deutsch</v-tab>
+    </v-tabs>
 
-  <p>The Uetliberg is constantly changing. Old paths become overgrown, new trails appear, or existing trails are temporarily closed for forestry works. I try to keep this map up to date, but I cannot give any guarantees about its accuracy. If you have any corrections for the map, then <nuxt-link to="contact">please let me know</nuxt-link>!</p>
+    <v-card-text>
+      <v-tabs-window v-model="language">
+        <v-tabs-window-item value="en">
+          <v-alert type="warning" variant="tonal">
+            I try to make the map as accurate and up-to-date as possible, but I cannot guarantee its correctness. I don't live in Zurich anymore since 2023, so I can't make very frequent updates anymore. Trails can increase in difficulty or become completely impassable at any time due to forestry work, landslides, etc. <nuxt-link to="contact">Please let me know</nuxt-link> if you have any corrections for the map!
+          </v-alert>
+          <v-alert type="warning" variant="tonal">
+            Don't underestimate the Uetliberg! Some of the trails on this map involve steep, technical terrain where a fall could have severe consequences. The ground often remains wet and slippery, even when it has not rained for a while. Trails are usually harder on the descent. When in doubt (especially when you have not done the trail before) pick an officially signposted hiking path or a well-maintained trail (Denzlerweg, Lindenweg) for the descent.
+          </v-alert>
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="de">
+          German
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card-text>
+  </v-card>
+
+  <p>The Uetliberg (the 870m tall hill rising above Zürich) is full of fascinating and sometimes surprisingly challenging unofficial hiking trails ("Trampelpfade"). Documenting these has been the masterwork of <nuxt-link to="http://www.hikr.org/user/Uto869/">Martin "Uto869"</nuxt-link> on Hikr. Inspired by his work, I have made an interactive map of all of his routes and even some additional trails. (<nuxt-link to="http://www.hikr.org/post115832.html">Announcement on Hikr</nuxt-link>)</p>
 
   <h3>Legend</h3>
   <ul class="uetlibergLegend">
@@ -49,7 +71,7 @@
     </li>
   </ul>
 
-  <p><nuxt-link to="https://map.geo.admin.ch/?topic=ech&lang=en&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.zeitreihen,ch.bfs.gebaeude_wohnungs_register,ch.bav.haltestellen-oev,ch.swisstopo.swisstlm3d-wanderwege,KML%7C%7Chttp:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Ftrampelpfade2.kml,KML%7C%7Chttps:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Futo869.kml&layers_visibility=false,false,false,true,true,true&layers_timestamp=18641231,,,,,&zoom=6&E=2680271.70&N=1244290.14">View on map.geo.admin.ch</nuxt-link> for more map options. Don't forget to try out the 3D mode!</p>
+  <p><nuxt-link to="https://map.geo.admin.ch/?topic=ech&lang=en&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.zeitreihen,ch.bfs.gebaeude_wohnungs_register,ch.bav.haltestellen-oev,ch.swisstopo.swisstlm3d-wanderwege,KML%7C%7Chttp:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Ftrampelpfade2.kml,KML%7C%7Chttps:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Futo869.kml&layers_visibility=false,false,false,true,true,true&layers_timestamp=18641231,,,,,&zoom=6&E=2680271.70&N=1244290.14">View on map.geo.admin.ch</nuxt-link> for more map options.</p>
 
   <iframe src="https://map.geo.admin.ch/embed.html?topic=ech&lang=en&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.zeitreihen,ch.bfs.gebaeude_wohnungs_register,ch.bav.haltestellen-oev,ch.swisstopo.swisstlm3d-wanderwege,KML%7C%7Chttp:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Ftrampelpfade2.kml,KML%7C%7Chttps:%2F%2Fstijnvermeeren.be%2Ffiles%2Fuetliberg%2Futo869.kml&layers_visibility=false,false,false,true,true&layers_timestamp=18641231,,,,&X=244290.14&Y=680271.70&zoom=6" width="100%" height="600" frameborder='0' style="border:0"></iframe>
 </template>
@@ -60,6 +82,8 @@ useMeta({
   description: 'An interactive map of the many fascinating and sometimes surprisingly challenging trails ("Trampelpfade") on the Uetliberg, the 870m tall hill rising above Zürich. Inspired by the detailed documentation of these trails by Martin "Uto869" on Hikr.',
   image: '/images/uetliberg/og_image.png'
 })
+
+const language = ref("en")
 
 definePageMeta({
   activeMenuLink: '/projects'
